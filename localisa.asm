cde2609e6976f6eaa3854b326b286ff79f461c2f
ZegesMenden <51548005+ZegesMenden@users.noreply.github.com>
2023-06-03 20:46:18 -0400

    add MOV and rename assembler to localisa

--- localisa.asm
+++ localisa.asm
@@ -16,32 +16,37 @@
 ;	0b0110	 N/A 	JALR 	ra rb 			-> set PC to rb and store PC+1 in ra
 ;	0b0111	 N/A 	BEQ 	ra rb   imm 	-> set PC to rb + imm and store PC+1 in ra if EQ flag is set
 ;	0b1000	 N/A 	BGR		ra rb   imm 	-> set PC to rb + imm and store PC+1 in ra if GR flag is set
+;	0b1001	 N/A 	MOV		ra rb       	-> copy the value in rb to ra
 ;	0b1110	 N/A 	LUI 	ra imm 			-> ra = imm<<6 this instruction is special because it only uses 3 bits in order to fit a 10 bit immediate. the format is 0b111|reg|imm|
 
 ; base instructions
 #ruledef {
-	add  r{reg_a:u3} r{reg_b:u3} r{reg_c:u3}		=> 0x0`4 @ reg_a @ reg_b @ 0b000 @ reg_c
-	adc  r{reg_a:u3} r{reg_b:u3} r{reg_c:u3}		=> 0x0`4 @ reg_a @ reg_b @ 0b001 @ reg_c
-	sub  r{reg_a:u3} r{reg_b:u3} r{reg_c:u3}		=> 0x0`4 @ reg_a @ reg_b @ 0b010 @ reg_c
-	subc r{reg_a:u3} r{reg_b:u3} r{reg_c:u3}		=> 0x0`4 @ reg_a @ reg_b @ 0b011 @ reg_c
-	nand r{reg_a:u3} r{reg_b:u3} r{reg_c:u3} 		=> 0x0`4 @ reg_a @ reg_b @ 0b100 @ reg_c
-	and	 r{reg_a:u3} r{reg_b:u3} r{reg_c:u3} 		=> 0x0`4 @ reg_a @ reg_b @ 0b101 @ reg_c
-	or	 r{reg_a:u3} r{reg_b:u3} r{reg_c:u3} 		=> 0x0`4 @ reg_a @ reg_b @ 0b110 @ reg_c
-	xor	 r{reg_a:u3} r{reg_b:u3} r{reg_c:u3} 		=> 0x0`4 @ reg_a @ reg_b @ 0b111 @ reg_c
-	addi r{reg_a:u3} r{reg_b:u3} i{imm:i6}  		=> 0x1`4 @ reg_a @ reg_b @ imm
-	bs	 r{reg_a:u3} r{reg_b:u3} i{imm:u6}			=> 0x2`4 @ reg_a @ reg_b @ imm
-	sw	 r{reg_a:u3} r{reg_b:u3} i{imm:s6} 			=> 0x3`4 @ reg_a @ reg_b @ imm
-	lw	 r{reg_a:u3} r{reg_b:u3} i{imm:s6}			=> 0x4`4 @ reg_a @ reg_b @ imm
-	cmp	 r{reg_a:u3} r{reg_b:u3}					=> 0x5`4 @ reg_a @ reg_b @ 0b000000
-	jalr r{reg_a:u3} r{reg_b:u3}					=> 0x6`4 @ reg_a @ reg_b @ 0b000000
-	beq	 r{reg_a:u3} r{reg_b:u3} i{imm:s6}			=> 0x7`4 @ reg_a @ reg_b @ imm
-	bgr	 r{reg_a:u3} r{reg_b:u3} i{imm:s6}			=> 0x8`4 @ reg_a @ reg_b @ imm
-	lui  r{reg_a:u3} i{imm: i10}					=> 0b111`3 @ reg_a @ imm
+	add  r{reg_a:u3} r{reg_b:u3} r{reg_c:u3}		=> 0x0 @ reg_a @ reg_b @ 0b000 @ reg_c
+	adc  r{reg_a:u3} r{reg_b:u3} r{reg_c:u3}		=> 0x0 @ reg_a @ reg_b @ 0b001 @ reg_c
+	sub  r{reg_a:u3} r{reg_b:u3} r{reg_c:u3}		=> 0x0 @ reg_a @ reg_b @ 0b010 @ reg_c
+	subc r{reg_a:u3} r{reg_b:u3} r{reg_c:u3}		=> 0x0 @ reg_a @ reg_b @ 0b011 @ reg_c
+	nand r{reg_a:u3} r{reg_b:u3} r{reg_c:u3} 		=> 0x0 @ reg_a @ reg_b @ 0b100 @ reg_c
+	and	 r{reg_a:u3} r{reg_b:u3} r{reg_c:u3} 		=> 0x0 @ reg_a @ reg_b @ 0b101 @ reg_c
+	or	 r{reg_a:u3} r{reg_b:u3} r{reg_c:u3} 		=> 0x0 @ reg_a @ reg_b @ 0b110 @ reg_c
+	xor	 r{reg_a:u3} r{reg_b:u3} r{reg_c:u3} 		=> 0x0 @ reg_a @ reg_b @ 0b111 @ reg_c
+	addi r{reg_a:u3} r{reg_b:u3} i{imm:i6}  		=> 0x1 @ reg_a @ reg_b @ imm
+	bs	 r{reg_a:u3} r{reg_b:u3} i{imm:u6}			=> 0x2 @ reg_a @ reg_b @ imm
+	sw	 r{reg_a:u3} r{reg_b:u3} i{imm:s6} 			=> 0x3 @ reg_a @ reg_b @ imm
+	lw	 r{reg_a:u3} r{reg_b:u3} i{imm:s6}			=> 0x4 @ reg_a @ reg_b @ imm
+	cmp	 r{reg_a:u3} r{reg_b:u3}					=> 0x5 @ reg_a @ reg_b @ 0b000000
+	jalr r{reg_a:u3} r{reg_b:u3}					=> 0x6 @ reg_a @ reg_b @ 0b000000
+	beq	 r{reg_a:u3} r{reg_b:u3} i{imm:s6}			=> 0x7 @ reg_a @ reg_b @ imm
+	bgr	 r{reg_a:u3} r{reg_b:u3} i{imm:s6}			=> 0x8 @ reg_a @ reg_b @ imm
+	mov  r{reg_a:u3} r{reg_b:u3}					=> 0x9 @ reg_a @ reg_b @ 0b000000
+	lui  r{reg_a:u3} i{imm: i10}					=> 0b111 @ reg_a @ imm
 }
 
 ; useful macros
 #ruledef {
 
+	; delay 1 cycle
+	nop => asm { addi r0 r0 i0 }
+cvc
 	; negate a number
 	not r{reg_a:u3} r{reg_b:u3} => asm { nand r reg_a r reg_b r reg_b }
 
@@ -54,11 +59,9 @@
 	bsr r{reg_a:u3} r{reg_b:u3} r{reg_c:u3} => 0x2 @ reg_a @ reg_b @ 0b100 @reg_c
 
 	; load immediate to register
-	ldi r{reg_a:u3} i{imm:i16} => asm {
-		lui r reg_a i ((imm & 0xffc0)>>6)`10
+	movi r{reg_a:u3} i{imm:i16} => asm {
+		lui r reg_a i (imm>>6)`10
 		addi r reg_a r reg_a i imm`6
 	}
 
 }
-
-ldi r0 i 0xffff
